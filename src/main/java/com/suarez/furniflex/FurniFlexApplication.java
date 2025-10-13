d16517486bb33a2234ca30cb5e3079d4c52fa9f2
LeasyLaige <lelouchiuvora243@gmail.com>
2025-10-13 21:58:33 +0800

    Initialize Spring Boot project with Maven and MySQL setup

---
+++ src/main/java/com/suarez/furniflex/FurniFlexApplication.java
@@ -0,0 +1,13 @@
+package com.suarez.furniflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurniFlexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FurniFlexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/suarez/furniflex/FurniFlexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suarez.furniflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurniFlexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
